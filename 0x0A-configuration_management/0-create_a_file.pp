3837fc9bbf872b5d7259c21dd2446e548112ca45
saidskander <askander199h@outlook.com>
2020-08-03 16:07:20 +0100

    Done

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # creat file in /tmp/holberton
 file {'/tmp/holberton',
   ensure => file,
-  mode => 0744,
-  owner => www-data,
-  group => www-data,
-  content => I love Puppet,
+  mode => '0744',
+  owner => 'www-data',
+  group => 'www-data',
+  content => 'I love Puppet',
 }
