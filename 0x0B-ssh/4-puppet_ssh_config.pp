c76be5fbc4e8318e5dc066b95e56a1c979a1091f
saidskander <askander199h@outlook.com>
2020-08-04 21:35:28 +0100

    Done

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -2,6 +2,6 @@
 
 exec { 'echo':
   path    => '/etc/ssh/sshd_config',
-  command => 'echo "IdentityFile ~/.ssh/holberton\n PasswordAuthentication no" >> /etc/ssh/sshd_config',
+  command => 'echo "  IdentityFile ~/.ssh/holberton\n  PasswordAuthentication no" >> /etc/ssh/sshd_config',
   returns => [0,1],
 }
