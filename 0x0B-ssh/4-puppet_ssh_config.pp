f42866a2aa1ed44bef4d2a7fda28a0de026b9c3b
saidskander <askander199h@outlook.com>
2020-08-04 21:32:14 +0100

    Done

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,7 @@
+# Changes SSH configguration file connecting to a server without using a password
+
+exec { 'echo':
+  path    => '/etc/ssh/sshd_config',
+  command => 'echo "IdentityFile ~/.ssh/holberton\n PasswordAuthentication no" >> /etc/ssh/sshd_config',
+  returns => [0,1],
+}
