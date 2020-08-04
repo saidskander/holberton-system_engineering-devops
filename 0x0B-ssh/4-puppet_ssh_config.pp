3b57ee081a3de06c3b0b940354642e9bf920ac3d
saidskander <askander199h@outlook.com>
2020-08-04 22:14:58 +0100

    Done

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,7 +1,12 @@
 # Changes SSH configguration file connecting to a server without using a password
 
-exec { 'echo':
-  path    => '/etc/ssh/sshd_config',
-  command => 'echo "  IdentityFile ~/.ssh/holberton\n  PasswordAuthentication no" >> /etc/ssh/sshd_config',
-  returns => [0,1],
+file_line {'Turn off passwd auth':
+    path   => '/etc/ssh/ssh_config',
+    line   => '	PasswordAuthentication no',
+    match  => 'PasswordAuthentication yes',
+}
+
+file_line {'Declare identity file':
+    path   => '/etc/ssh/ssh_config',
+    line   => '	IdentityFile ~/.ssh/holberton',
 }
