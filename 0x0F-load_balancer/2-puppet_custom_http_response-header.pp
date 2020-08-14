66fde4ddbae65f325479a36a97599da608703d0b
saidskander <askander199h@outlook.com>
2020-08-14 23:44:02 +0100

    Done

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -0,0 +1,19 @@
+# creat http Header 
+exec {'ensire update':
+command  => 'sudo apt update',
+provider => shell
+}
+
+-> package {'nginx':
+ensure   => 'installed'
+}
+
+-> file_line {'header':
+path     => '/etc/nginx/nginx.conf',
+match    => 'http {',
+line     => "http {\n\tadd_header X-Served-By ${hostname};"
+}
+
+-> exec {'start':
+command  => '/usr/sbin/service nginx restart'
+}
