458b569b02073b5184824c87b97924c1a90e7f7c
saidskander <askander199h@outlook.com>
2020-09-23 22:47:09 +0100

    Done

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,10 +6,10 @@ import requests
 def number_of_subscribers(subreddit):
     """number_of_subscribers """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
-             AppleWebKit/537.36 (KHTML, like Gecko)\
-             Chrome/85.0.4183.121 Safari/537.36"
-    headers = {"User-Agent": agent}
+
+    headers = {
+        "User-Agent": "win64:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
     x = requests.get(url, headers=headers).json().get('data')
     if x:
         return x.get('subscribers')
