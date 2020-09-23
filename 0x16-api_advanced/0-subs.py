1818cb5e03be5f81d43f164faf9377e35a9ed91d
saidskander <askander199h@outlook.com>
2020-09-23 23:19:33 +0100

    Done

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,8 +9,8 @@ def number_of_subscribers(subreddit):
 
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
-        AppleWebKit/537.36 (KHTML, like Gecko)\
-        Chrome/85.0.4183.121 Safari/537.36"
+         AppleWebKit/537.36 (KHTML, like Gecko)\
+         Chrome/85.0.4183.121 Safari/537.36"
     }
     x = requests.get(url, headers=headers, allow_redirects=False)
     if x.status_code == 404:
