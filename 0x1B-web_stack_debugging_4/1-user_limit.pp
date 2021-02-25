a1cbfbd4519ea506441f9c3a7b4bcf021e873ec1
saidskander <askander199h@outlook.com>
2021-02-25 21:38:56 +0200

    Done

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,3 +1,3 @@
 # puppet for a user limit
 
-exec { '/usr/bin/env sed -i "s/holberton/foo" /etc/security/limits.conf': }
+exec { '/usr/bin/env sed -i "s/holberton/foo/" /etc/security/limits.conf': }
